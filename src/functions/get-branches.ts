d9a1217cca9fa79f03825da81de61049d021676f
yuvraj shekhawat <shekhawat.yuvraj@grofers.com>
2025-07-02 19:48:31 +0530

    update error logs

--- src/functions/get-branches.ts
+++ src/functions/get-branches.ts
@@ -3,6 +3,7 @@ import * as core from '@actions/core'
 import {github, owner, repo} from './get-context'
 import {BranchResponse} from '../types/branches'
 import {logGetBranches} from './logging/log-get-branches'
+import * as fs from 'fs'
 
 /**
  * Retrieves all branches in a repository
@@ -43,10 +44,14 @@ export async function getBranches(includeProtectedBranches: boolean): Promise<Br
     core.info(logGetBranches(branches.length))
   } catch (err) {
     if (err instanceof Error) {
+      fs.writeFileSync('/tmp/error_response.log', JSON.stringify(errorLog, null, 2))
       core.error(`Full error object: ${JSON.stringify(err, Object.getOwnPropertyNames(err), 2)}`)
+      core.error('Full error object and response data saved to error_response.log')
       core.setFailed(`Failed to retrieve branches for ${repo}. Error: ${err.message}`)
     } else {
+      fs.writeFileSync('/tmp/error_response.log', JSON.stringify({error: JSON.stringify(err)}, null, 2))
       core.error(`Full error object: ${JSON.stringify(err)}`)
+      core.error('Full error object saved to error_response.log')
       core.setFailed(`Failed to retrieve branches for ${repo}.`)
     }
     branches = [{branchName: '', commmitSha: ''}]
